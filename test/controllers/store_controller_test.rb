f6a9fe4d4328a99cff893623881596367703aa29
Landon West <landonwest5@gmail.com>
2016-04-14 11:37:38 -0700

    configure product price format; set tests for store controller

--- test/controllers/store_controller_test.rb
+++ test/controllers/store_controller_test.rb
@@ -1,9 +1,13 @@
 require 'test_helper'
-
+#these tests are based off the fixture data we entered in test/fixtures/products.yml
 class StoreControllerTest < ActionController::TestCase
   test "should get index" do
     get :index
     assert_response :success
+    assert_select '#columns #side a', minimum: 4
+    assert_select '#main .entry', 2
+    assert_select 'h3', 'M1911'
+    assert_select '.price', /\$[,\d]+\.\d\d/
   end
 
 end
