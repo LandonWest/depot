c83ca57e4ec219bea88b1ca560b7d495188d4a77
Landon West <landonwest5@gmail.com>
2016-04-14 19:42:55 -0700

    create cart scaffold, migrate db; create CurrentCart module to set cart in session

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160412183911) do
+ActiveRecord::Schema.define(version: 20160415011411) do
+
+  create_table "carts", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "products", force: true do |t|
     t.string   "title"
